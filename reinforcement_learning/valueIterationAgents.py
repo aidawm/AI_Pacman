684fa4740429ead6ad855c1bfe3eaf8b0d98c90a
Aida Mobli <aidaw.mobli@gmail.com>
2023-05-30 20:20:16 +0330

    synchronous value iteration & asynchronous value iteration is done

--- reinforcement_learning/valueIterationAgents.py
+++ reinforcement_learning/valueIterationAgents.py
@@ -40,7 +40,8 @@ class ValueIterationAgent(ValueEstimationAgent):
         for a given number of iterations using the supplied
         discount factor.
     """
-    def __init__(self, mdp, discount = 0.9, iterations = 100):
+
+    def __init__(self, mdp, discount=0.9, iterations=100):
         """
           Your value iteration agent should take an mdp on
           construction, run the indicated number of iterations
@@ -56,13 +57,27 @@ class ValueIterationAgent(ValueEstimationAgent):
         self.mdp = mdp
         self.discount = discount
         self.iterations = iterations
-        self.values = util.Counter() # A Counter is a dict with default 0
+        self.values = util.Counter()  # A Counter is a dict with default 0
         self.runValueIteration()
 
     def runValueIteration(self):
-        # Write value iteration code here
-        "*** YOUR CODE HERE ***"
-
+        states = self.mdp.getStates()
+        for k in range(self.iterations):
+            next_values = util.Counter()
+            for s in states:
+                actions = self.mdp.getPossibleActions(s)
+                max_value_s = -9999
+                for a in actions:
+                    transition = self.mdp.getTransitionStatesAndProbs(s, a)
+                    sum_of_values = 0.0
+                    for state_prob in transition:
+                        sum_of_values += state_prob[1] * (self.mdp.getReward(s, a, state_prob[0]) + (self.discount * self.values[state_prob[0]]))
+                    max_value_s = max(max_value_s, sum_of_values)
+                if max_value_s != -9999:
+                    next_values[s] = max_value_s
+                    
+            for s in states:
+                self.values[s] = next_values[s]
 
     def getValue(self, state):
         """
@@ -70,14 +85,16 @@ class ValueIterationAgent(ValueEstimationAgent):
         """
         return self.values[state]
 
-
     def computeQValueFromValues(self, state, action):
         """
           Compute the Q-value of action in state from the
           value function stored in self.values.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        value = 0.0
+        for stateProb in self.mdp.getTransitionStatesAndProbs(state, action):
+            value += stateProb[1] * (self.mdp.getReward(state, action, stateProb[0]) + self.discount * self.values[stateProb[0]])
+
+        return value
 
     def computeActionFromValues(self, state):
         """
@@ -88,8 +105,15 @@ class ValueIterationAgent(ValueEstimationAgent):
           there are no legal actions, which is the case at the
           terminal state, you should return None.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        max_value = -99999
+        max_action = None
+
+        for action in self.mdp.getPossibleActions(state):
+            action_value = self.computeQValueFromValues(state, action)
+            if action_value > max_value:
+                max_value = action_value
+                max_action = action
+        return max_action
 
     def getPolicy(self, state):
         return self.computeActionFromValues(state)
@@ -100,6 +124,7 @@ class ValueIterationAgent(ValueEstimationAgent):
 
     def getQValue(self, state, action):
         return self.computeQValueFromValues(state, action)
+
 
 class AsynchronousValueIterationAgent(ValueIterationAgent):
     """
@@ -130,7 +155,17 @@ class AsynchronousValueIterationAgent(ValueIterationAgent):
 
     def runValueIteration(self):
         "*** YOUR CODE HERE ***"
+        states = self.mdp.getStates()
 
+        for k in range(self.iterations):
+            state = states[k % len(states)]
+            if self.mdp.isTerminal(state):
+                continue
+            q_value = list()
+            for action in self.mdp.getPossibleActions(state):
+                q_value.append(self.getQValue(state, action))
+            self.values[state] = max(q_value)
+        
 class PrioritizedSweepingValueIterationAgent(AsynchronousValueIterationAgent):
     """
         * Please read learningAgents.py before reading this.*
